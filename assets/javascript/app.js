6db69a66ab89411935b6c1e8277c574137e67b47
naim-ea <elayadinaim@gmail.com>
2017-12-19 09:48:44 +0100

    :art: Transitions between screens

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,29 @@
-console.log('Hello')
+const DOM = {};
+
+DOM.country_backtomap     = document.querySelector('.single-country-backtomap');
+DOM.place_backtomap       = document.querySelector('.single-place-footer-backtomap');
+DOM.place_backtocountry   = document.querySelector('.single-place-backtocountry');
+DOM.country_place_choice  = document.querySelectorAll('.single-country-discover-element-content-discover');
+
+DOM.country               = document.querySelector('.single-country');
+DOM.place                 = document.querySelector('.single-place');
+
+
+DOM.country_backtomap.addEventListener('click', () => {
+    DOM.country.classList.remove('active');
+});
+
+DOM.place_backtomap.addEventListener('click', () => {
+    DOM.country.classList.remove('active');
+    DOM.place.classList.remove('active');
+});
+
+DOM.place_backtocountry.addEventListener('click', () => {
+    DOM.place.classList.remove('active');
+});
+
+DOM.country_place_choice.forEach(element => {
+    element.addEventListener('click', () => {
+        DOM.place.classList.add('active');
+    })
+});
