f07d49a781af028ea5ce247a9f92ddb647b89ae0
naim-ea <elayadinaim@gmail.com>
2017-12-19 15:33:33 +0100

    :art: Place page remodel in progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,47 @@
-const DOM = {};
+// const DOM = {};
 
-DOM.country_backtomap     = document.querySelector('.single-country-backtomap');
-DOM.place_backtomap       = document.querySelector('.single-place-footer-backtomap');
-DOM.place_backtocountry   = document.querySelector('.single-place-backtocountry');
-DOM.country_place_choice  = document.querySelectorAll('.single-country-discover-element-content-discover');
+// DOM.country_backtomap     = document.querySelector('.single-country-backtomap');
+// DOM.place_backtomap       = document.querySelector('.single-place-footer-backtomap');
+// DOM.place_backtocountry   = document.querySelector('.single-place-backtocountry');
+// DOM.country_place_choice  = document.querySelectorAll('.single-country-discover-element-content-discover');
 
-DOM.country               = document.querySelector('.single-country');
-DOM.place                 = document.querySelector('.single-place');
+// DOM.country               = document.querySelector('.single-country');
+// DOM.place                 = document.querySelector('.single-place');
 
 
-DOM.country_backtomap.addEventListener('click', () => {
-    DOM.country.classList.remove('active');
-});
+// DOM.country_backtomap.addEventListener('click', () => {
+//     DOM.country.classList.remove('active');
+// });
 
-DOM.place_backtomap.addEventListener('click', () => {
-    DOM.country.classList.remove('active');
-    DOM.place.classList.remove('active');
-});
+// DOM.place_backtomap.addEventListener('click', () => {
+//     DOM.country.classList.remove('active');
+//     DOM.place.classList.remove('active');
+// });
 
-DOM.place_backtocountry.addEventListener('click', () => {
-    DOM.place.classList.remove('active');
-});
+// DOM.place_backtocountry.addEventListener('click', () => {
+//     DOM.place.classList.remove('active');
+// });
 
-DOM.country_place_choice.forEach(element => {
-    element.addEventListener('click', () => {
-        DOM.place.classList.add('active');
-    })
-});
+// DOM.country_place_choice.forEach(element => {
+//     element.addEventListener('click', () => {
+//         DOM.place.classList.add('active');
+//     })
+// });
+
+/********
+ * MENU
+ * 
+ */
+
+ let menu = document.querySelector('.menu-hamburger');
+ let menu_content = document.querySelector('.menu-bg');
+ menu.addEventListener('click', () => {
+    if(menu.classList.contains('active')){
+        menu.classList.remove('active');
+        menu_content.classList.remove('active');
+    }
+    else{
+        menu.classList.add('active');
+        menu_content.classList.add('active');
+    }
+ });
